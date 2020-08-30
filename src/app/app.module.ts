7af545a7e286dc6495eda3c459f04ec10a3f8942
Marcio Fernandes <marciofso@hotmail.com>
2020-08-30 15:59:41 -0300

    Mostrando dados e imagem do cliente para a pagina de profile

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,7 @@ import { CategoriaService } from '../services/domain/categoria.service';
 import { ErrorInterceptorProvider } from '../interceptors/error-interceptor';
 import { AuthService } from '../services/auth.service';
 import { StorageService } from '../services/storage.service';
+import { ClienteService } from '../services/domain/cliente.service';
 
 @NgModule({
   declarations: [
@@ -33,7 +34,8 @@ import { StorageService } from '../services/storage.service';
     CategoriaService,
     ErrorInterceptorProvider,
     AuthService, 
-    StorageService
+    StorageService,
+    ClienteService
   ]
 })
 export class AppModule {}
