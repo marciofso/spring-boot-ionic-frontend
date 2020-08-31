539c4ea9fd998494826ddae30c614133b14aa832
Marcio Fernandes <marciofso@hotmail.com>
2020-08-31 14:59:00 -0300

    Interceptor para incluir token nas requisicoes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,7 @@ import { ErrorInterceptorProvider } from '../interceptors/error-interceptor';
 import { AuthService } from '../services/auth.service';
 import { StorageService } from '../services/storage.service';
 import { ClienteService } from '../services/domain/cliente.service';
+import { AuthInterceptorProvider } from '../interceptors/auth-interceptor';
 
 @NgModule({
   declarations: [
@@ -32,6 +33,7 @@ import { ClienteService } from '../services/domain/cliente.service';
     SplashScreen,
     {provide: ErrorHandler, useClass: IonicErrorHandler},
     CategoriaService,
+    AuthInterceptorProvider,
     ErrorInterceptorProvider,
     AuthService, 
     StorageService,
--- src/services/domain/cliente.service.ts
+++ src/services/domain/cliente.service.ts
@@ -12,15 +12,8 @@ export class ClienteService {
     }
 
         findByEmail(email: String) : Observable<ClienteDTO> {
-            
-            let token = this.storage.getLocalUser().token;
-            let authHeader = new HttpHeaders({'Authorization': 'Bearer ' + token});
-            
-            return this.http.get<ClienteDTO>(
-                `${API_CONFIG.baseUrl}/clientes/email?value=${email}`,
-                {'headers': authHeader});
-
-        }
+            return this.http.get<ClienteDTO>(`${API_CONFIG.baseUrl}/clientes/email?value=${email}`);
+         }
 
         getImageFromBucket(id : string) : Observable<any> {
             let url = `${API_CONFIG.bucketBaseUrl}/cp${id}.jpg`
