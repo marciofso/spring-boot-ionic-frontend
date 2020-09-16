1ee094d1f0caa9d7f4eee44dfa5ab324860bba9f
Marcio Fernandes <marciofso@hotmail.com>
2020-09-16 18:55:20 -0300

    Tela de escolha de pagamento

--- src/pages/pickaddress/pickaddress.ts
+++ src/pages/pickaddress/pickaddress.ts
@@ -56,7 +56,7 @@ export class PickaddressPage {
 
 nextPage(item: EnderecoDTO) {
   this.pedido.enderecoDeEntrega = {id: item.id};
-  console.log(this.pedido);
+  this.navCtrl.push('PaymentPage', {pedido: this.pedido});
 }
 
 }
