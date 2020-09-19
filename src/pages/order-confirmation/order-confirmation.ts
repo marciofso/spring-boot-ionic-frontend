20cc40413d0d064defd59f2f334d31c8e2f3fcea
Marcio Fernandes <marciofso@hotmail.com>
2020-09-19 17:06:37 -0300

    Informando perdido resgistrado ao usuario

--- src/pages/order-confirmation/order-confirmation.ts
+++ src/pages/order-confirmation/order-confirmation.ts
@@ -19,6 +19,7 @@ export class OrderConfirmationPage {
   cartItems: CartItem[];
   cliente: ClienteDTO;
   endereco: EnderecoDTO;
+  codpedido: string;
 
   constructor(
     public navCtrl: NavController, 
@@ -57,11 +58,15 @@ export class OrderConfirmationPage {
     this.navCtrl.setRoot('CartPage');
   }
 
+  home() {
+    this.navCtrl.setRoot('CategoriasPage');
+  }
+
   checkout() {
     this.pedidoService.insert(this.pedido)
        .subscribe(response => {
          this.cartService.createOrClearCart();
-         console.log(response.headers.get('location'));
+         this.codpedido = this.extractId(response.headers.get('location'));
        },
        error => {
          if (error.status == 403) {
@@ -69,6 +74,11 @@ export class OrderConfirmationPage {
          }
        });
 
+  }
+
+  private extractId(location : string) : string {
+    let position = location.lastIndexOf('/');
+    return location.substring(position + 1, location.length);
   }
 
 }
