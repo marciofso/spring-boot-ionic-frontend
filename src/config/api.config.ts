f12f5d833c489343734a983701fc98129001c39e
Marcio Fernandes <marciofso@hotmail.com>
2020-09-29 19:04:21 -0300

    Testando o app no dispositivo

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,4 +1,4 @@
 export const API_CONFIG = {
-    baseUrl: "http://localhost:8080", 
+    baseUrl: "https://spring-boot-ionic-backend-mfso.herokuapp.com", 
     bucketBaseUrl: "https://curso-spring-ionic-marciofso.s3-sa-east-1.amazonaws.com"
 }
