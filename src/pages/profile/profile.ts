42336fd3b4a106824973d5278e8afa8fd83333a3
Marcio Fernandes <marciofso@hotmail.com>
2020-09-30 16:32:02 -0300

    Bugfix - cancelamento da camera ou galeria

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -72,6 +72,7 @@ export class ProfilePage {
      this.picture = 'data:image/png;base64,' + imageData;
      this.cameraOn = false;
     }, (err) => {
+      this.cameraOn = false;
     });
   }
 
@@ -91,6 +92,7 @@ export class ProfilePage {
      this.picture = 'data:image/png;base64,' + imageData;
      this.cameraOn = false;
     }, (err) => {
+      this.cameraOn = false;
     });
   }
 
